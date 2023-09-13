f0dae71a6080a784feb6fd70a91b2ceec41a0c80
Mike Stromme <mikestromme@gmail.com>
2023-09-13 15:09:19 -0500

    x

--- my_whisper.py
+++ my_whisper.py
@@ -8,15 +8,32 @@ def transcribe_audio(audio_file):
     model = whisper.load_model("base")
     result = model.transcribe(audio_file)
     output = result["text"]
-    return output
+    processed_text = "This is an example of processed text."
+    return processed_text
+
+def download_link(text):
+    with open("processed_output.txt", "w") as f:
+        f.write(text)
+    return "processed_output.txt"
 
 def main():
     audio_input = gr.inputs.Audio(source="upload", type="filepath")
     output_text = gr.outputs.Textbox()
+    download_output = gr.outputs.Textbox(label="Download Link")
     
-    iface = gr.Interface(fn=transcribe_audio, inputs=audio_input, 
-                         outputs=output_text, title="Audio Transcription App",
-                         description="Upload an audio file and hit the 'Submit' button")
+    # iface = gr.Interface(fn=transcribe_audio, inputs=audio_input, 
+    #                      outputs=output_text, title="Audio Transcription App",
+    #                      description="Upload an audio file and hit the 'Submit' button")
+    
+
+    iface = gr.Interface(
+    fn=transcribe_audio,
+    inputs=audio_input,
+    outputs=[output_text, download_output],
+    title="Audio Transcription App",
+    description="Upload an audio file and hit the 'Submit' button"
+
+)
     
     iface.launch()
 
